110fe0840bf782656d3cb4a428e2ba847de05977
Justin Espedal <justin.espedal@gmail.com>
2016-09-30 07:04:58 -0700

    Pull in scaleMode update from stencyl engine.

--- engine/src/scripts/MyAssets.hx
+++ engine/src/scripts/MyAssets.hx
@@ -7,10 +7,7 @@ class MyAssets implements AssetLoader
 	//Game
 	public static var landscape:Bool;
 	public static var autorotate:Bool;
-	public static var stretchToFit:Bool;
-	public static var scaleToFit1:Bool;
-	public static var scaleToFit2:Bool;
-	public static var scaleToFit3:Bool;
+	public static var scaleMode:Int;
 	public static var stageWidth:Int;
 	public static var stageHeight:Int;
 	public static var initSceneID:Int;
@@ -20,7 +17,6 @@ class MyAssets implements AssetLoader
 	public static var antialias:Bool;
 	public static var pixelsnap:Bool;
 	public static var startInFullScreen:Bool;
-	public static var scaleMode:Int;
 
 	//APIs
 	public static var adPositionBottom:Bool;
